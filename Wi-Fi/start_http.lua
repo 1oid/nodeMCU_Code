e733bc28469bdb2707b7d5ed47528a973983b812
Loid <1398231927@qq.com>
2018-10-14 23:11:34 +0800

    Add files via upload

--- Wi-Fi/start_http.lua
+++ Wi-Fi/start_http.lua
@@ -10,16 +10,21 @@ end)
 
 httpServer:use('/config', function(req, res)
     if req.query.ssid ~= nil and req.query.pwd ~= nil then
+        wifi.sta.disconnect()
         local cfg = {}
         cfg.ssid = req.query.ssid
         cfg.pwd = req.query.pwd
         wifi.sta.config(cfg)
         print("connect wifi ssid: "..cfg.ssid.." pwd: "..cfg.pwd)
-        status = 'STA_CONNECTING'
-        tmr.alarm(TMR_WIFI, 1000, tmr.ALARM_AUTO, function()
-            if status ~= 'STA_CONNECTING' then
-                res:type('application/json')
-                res:send('{"status":"' .. status .. '"}')
+
+        -- 等待10秒连接wifi,这里可以自己修改
+        tmr.alarm(TMR_WIFI, 10000, tmr.ALARM_AUTO, function()
+            print(wifi.sta.getip())
+            if wifi.sta.getip() ~= nil then
+                res:send('{"status":"connect success."}')
+                tmr.stop(TMR_WIFI)
+            else
+                res:send('{"status":"connect fail."}')
                 tmr.stop(TMR_WIFI)
             end
         end)
